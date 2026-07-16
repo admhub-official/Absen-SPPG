ffd346a558333b3e1ecc6f9cc5b89df584d029c5
ADMIN <adminhub.official@gmail.com>
2026-07-16 12:13:15 +0700

    Load payroll and access features

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,12 @@ window.ABSEN_SUPABASE_CONFIG = window.ABSEN_SUPABASE_CONFIG || {
   projectUrl: 'https://szwwpnbbsmjsbzzcecyj.supabase.co',
   functionUrl: 'https://szwwpnbbsmjsbzzcecyj.supabase.co/functions/v1/Absen',
   functionName: 'Absen',
-  apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6d3dwbmJic21qc2J6emNlY3lqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwOTMwOTAsImV4cCI6MjA5ODY2OTA5MH0.xZo9tNJc6M7hNL4EMmmGab1hdq11c6N6GwH98Re3vX8'
+  apiKey: ''
 };
+
+(function () {
+  var script = document.createElement('script');
+  script.src = './features.js?v=20260716';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
