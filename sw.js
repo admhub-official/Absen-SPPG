069fd84f67f2533542ef6b255f9ae995da314a6c
ADMIN <adminhub.official@gmail.com>
2026-08-06 07:15:17 +0700

    chore(pwa): refresh dashboard shell cache v42

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'absen-sppg-shell-v41';
-const APP_VERSION = '26.11.1';
+const CACHE = 'absen-sppg-shell-v42';
+const APP_VERSION = '26.11.2';
 const CANONICAL_ORIGIN = 'https://hadirly.org';
 const LEGACY_HOSTS = new Set(['absen-sppg.pages.dev']);
 const SHELL = [
