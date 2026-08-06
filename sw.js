79f2e1aef0832894503cd4989586a4bb863124c2
ADMIN <adminhub.official@gmail.com>
2026-08-06 23:47:41 +0700

    chore(pwa): refresh complaint and modal assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'absen-sppg-hadirly-v73';
-const APP_VERSION = '26.11.32';
+const CACHE = 'absen-sppg-hadirly-v74';
+const APP_VERSION = '26.11.33';
 const CANONICAL_ORIGIN = 'https://hadirly.org';
 const LEGACY_HOSTS = new Set(['absen-sppg.pages.dev']);
 const SHELL = [
