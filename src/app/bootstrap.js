e5dd90aaa1e245182937029e6e6c15594cb6fa63
ADMIN <adminhub.official@gmail.com>
2026-08-06 15:02:09 +0700

    chore: bump frontend asset version

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -19,7 +19,7 @@ import { renderReleaseOperationsPage } from '../pages/release/release-operations
 import { renderWorkforceOperationsPage } from '../pages/workforce/workforce-operations-page.js';
 import { renderPlatformOperationsPage } from '../pages/platform/platform-operations-page.js';
 
-const VERSION = '26.11.24';
+const VERSION = '26.11.25';
 const loadedAssets = new Map();
 
 function canonicalPath(value) { return new URL(value, document.baseURI).pathname; }
