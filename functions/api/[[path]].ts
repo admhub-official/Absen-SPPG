a9613bdf4036be7c8d345ff7a32990a4a4f48a74
ADMIN <adminhub.official@gmail.com>
2026-08-08 10:57:51 +0700

    security: deploy same-origin BFF through Cloudflare Pages Functions

---
+++ functions/api/[[path]].ts
@@ -0,0 +1,13 @@
+import worker from "../../bff/cloudflare/worker.ts";
+
+const RUNTIME_DEFAULTS = Object.freeze({
+  HADIRLY_ORIGIN: "https://hadirly.org",
+  SUPABASE_URL: "https://szwwpnbbsmjsbzzcecyj.supabase.co",
+  SESSION_MAX_AGE_SECONDS: "28800",
+  ALLOW_LEGACY_EXCHANGE: "true",
+});
+
+export const onRequest = async (context: any) => {
+  const env = { ...RUNTIME_DEFAULTS, ...(context?.env || {}) };
+  return worker.fetch(context.request, env);
+};
