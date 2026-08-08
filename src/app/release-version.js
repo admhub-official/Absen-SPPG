cedbfd5995c3f2ba856ce7c81cc56811627974b8
ADMIN <adminhub.official@gmail.com>
2026-08-08 16:18:30 +0700

    chore(release): bump API consistency release

--- src/app/release-version.js
+++ src/app/release-version.js
@@ -1,6 +1,6 @@
 (() => {
-  const version = '26.11.65';
-  const cacheName = 'absen-sppg-hadirly-v106';
+  const version = '26.11.66';
+  const cacheName = 'absen-sppg-hadirly-v107';
   globalThis.HADIRLY_RELEASE = Object.freeze({ version, cacheName });
 })();
 
