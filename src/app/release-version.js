7cf7e01d73c36a9fc271bb43c42b78a0158086ad
ADMIN <adminhub.official@gmail.com>
2026-08-08 10:58:43 +0700

    chore: bump release for HttpOnly session cutover

--- src/app/release-version.js
+++ src/app/release-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '26.11.52';
-  const cacheName = 'absen-sppg-hadirly-v93';
+  const version = '26.11.53';
+  const cacheName = 'absen-sppg-hadirly-v94';
   globalThis.HADIRLY_RELEASE = Object.freeze({ version, cacheName });
 })();
