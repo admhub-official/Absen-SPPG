d3e0d2d01f745617f9629e490ef15ad6ae59fa65
ADMIN <adminhub.official@gmail.com>
2026-08-08 11:25:07 +0700

    chore: bump PWA release after session hardening review

--- src/app/release-version.js
+++ src/app/release-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '26.11.53';
-  const cacheName = 'absen-sppg-hadirly-v94';
+  const version = '26.11.54';
+  const cacheName = 'absen-sppg-hadirly-v95';
   globalThis.HADIRLY_RELEASE = Object.freeze({ version, cacheName });
 })();
